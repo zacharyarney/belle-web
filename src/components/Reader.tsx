f01ec8a907e9883cd8a7a342d955bd45ac5db71f
Zachary Arney <zachary.arney@gmail.com>
2024-08-14 22:48:56 -0700

    Makes reader more readable

--- src/components/Reader.tsx
+++ src/components/Reader.tsx
@@ -14,15 +14,33 @@ export default function Reader() {
   const handlePlayText = () => setIsPlaying(!isPlaying);
 
   const currentWord = wordArray[textIndex] ? (
-    <p className="text-xl">{wordArray[textIndex]}</p>
+    <div className="text-xl flex flex-col items-start">
+      <p>
+        <span className="text-red-400">
+          {wordArray[textIndex].substring(
+            0,
+            Math.min(3, Math.ceil(wordArray[textIndex].length / 2))
+          )}
+        </span>
+        <span>
+          {wordArray[textIndex].substring(
+            Math.min(3, Math.ceil(wordArray[textIndex].length / 2))
+          )}
+        </span>
+      </p>
+      <p className="text-red-400">^</p>
+    </div>
   ) : (
-    <p className="text-xl">&nbsp;</p>
+    <div className="text-xl flex flex-col items-start">
+      <p className="text-xl">&nbsp;</p>
+      <p className="text-xl">&nbsp;</p>
+    </div>
   );
   const playButtonText = isPlaying ? 'pause' : 'play';
 
   return (
     <div className="w-full flex flex-wrap justify-between space-y-4">
-      <div className="w-full border-neutral-200 border-2 rounded py-14">
+      <div className="w-full border-neutral-200 border-2 rounded pt-14 pb-7 px-28">
         {currentWord}
       </div>
       <Button onClick={handlePlayText}>{playButtonText}</Button>
